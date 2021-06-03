e803aa59d3f443a83b32928e4fcabb57b42434b9
zoebentley <37678690+zoebentley@users.noreply.github.com>
2021-06-03 16:56:34 -0400

    Update MimiPastel_theme.js
    Added secondary and tertiary

--- src/themes/MimiPastel_theme.js
+++ src/themes/MimiPastel_theme.js
@@ -5,61 +5,61 @@ export default Blockly.Theme.defineTheme('MimiPastelTheme', {
     'blockStyles': {
         "motion": {
           "colourPrimary": "#77b7f9",
-          "colourSecondary": "#77b7f9",
-          "colourTertiary": "#77b7f9"
+          "colourSecondary": "#679fd6",
+          "colourTertiary": "#5684b3"
         },
         "looks": {
           "colourPrimary": "#b6a8f9",
-          "colourSecondary": "#b6a8f9",
-          "colourTertiary": "#b6a8f9"
+          "colourSecondary": "#9b90d6",
+          "colourTertiary": "#8078b3"
         },
         "sounds": {
           "colourPrimary": "#e597ed",
-          "colourSecondary": "#e597ed",
-          "colourTertiary": "#e597ed"
+          "colourSecondary": "#c07fc7",
+          "colourTertiary": "#936299"
         },
         "control": {
           "colourPrimary": "#f8ad40",
-          "colourSecondary": "#f8ad40",
-          "colourTertiary": "#f8ad40"
+          "colourSecondary": "#cc9035",
+          "colourTertiary": "#a6752b"
         },
         "event": {
           "colourPrimary": "#fac03f",
-          "colourSecondary": "#fac03f",
-          "colourTertiary": "#fac03f"
+          "colourSecondary": "#cc9c33",
+          "colourTertiary": "#a67e29"
         },
         "sensing": {
           "colourPrimary": "#87cde2",
-          "colourSecondary": "#87cde2",
-          "colourTertiary": "#87cde2"
+          "colourSecondary": "#71abbd",
+          "colourTertiary": "#5c8b99"
         },
         "pen": {
           "colourPrimary": "#5ed8a6",
-          "colourSecondary": "#5ed8a6",
-          "colourTertiary": "#5ed8a6"
+          "colourSecondary": "#4fb389",
+          "colourTertiary": "#3e8c6c"
         },
         "operators": {
           "colourPrimary": "#7dcd74",
-          "colourSecondary": "#7dcd74",
-          "colourTertiary": "#7dcd74"
+          "colourSecondary": "#66a65e",
+          "colourTertiary": "#4f8049"
         },
         "data": {
           "colourPrimary": "#f7a668",
-          "colourSecondary": "#f7a668",
-          "colourTertiary": "#f7a668"
+          "colourSecondary": "#cc8956",
+          "colourTertiary": "#a66f46"
           // text color #2a1c47
         },
         // This is not a new category, but rather for differentiation
         // between lists and scalar variables.
         "data_lists": {
           "colourPrimary": "#f58968",
-          "colourSecondary": "#f58968",
-          "colourTertiary": "#f58968"
+          "colourSecondary": "#cc7156",
+          "colourTertiary": "#a65c46"
         },
         "more": {
           "colourPrimary": "#f58da5",
-          "colourSecondary": "#f58da5",
-          "colourTertiary": "#f58da5"
+          "colourSecondary": "#cc768a",
+          "colourTertiary": "#a66070"
       },
       "text": {
         "colourPrimary": "#454b5a",
