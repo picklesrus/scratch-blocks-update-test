15a5d951ecd70cbd698ef568c1a3e8b8bc5db0e0
zoebentley <37678690+zoebentley@users.noreply.github.com>
2021-06-03 16:48:25 -0400

    Update EricR_theme.js
    Add secondary and tertiary

--- src/themes/EricR_theme.js
+++ src/themes/EricR_theme.js
@@ -4,61 +4,61 @@ export default Blockly.Theme.defineTheme('EricRTheme', {
     'base': Blockly.Themes.Classic,
     'blockStyles': {
         "motion": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#004099",
+          "colourSecondary": "#003073",
+          "colourTertiary": "#00204d"
         },
         "looks": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#220066",
+          "colourSecondary": "#150040",
+          "colourTertiary": "#08001a"
         },
         "sounds": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#752475",
+          "colourSecondary": "#331033",
+          "colourTertiary": "#0d040d"
         },
         "control": {
           "colourPrimary": "#664100",
-          "colourSecondary": "#664100",
-          "colourTertiary": "#664100"
+          "colourSecondary": "#402800",
+          "colourTertiary": "#1a1000"
         },
         "event": {
           "colourPrimary": "#997300",
-          "colourSecondary": "#997300",
-          "colourTertiary": "#997300"
+          "colourSecondary": "#594300",
+          "colourTertiary": "#332600"
         },
         "sensing": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#1f5f7a",
+          "colourSecondary": "#133b4d",
+          "colourTertiary": "#0a1d26"
         },
         "pen": {
-          "colourPrimary": "#0fBD8C",
-          "colourSecondary": "#0DA57A",
-          "colourTertiary": "#0B8E69"
+          "colourPrimary": "#0a805e",
+          "colourSecondary": "#075942",
+          "colourTertiary": "#043326"
         },
         "operators": {
           "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourSecondary": "#133313",
+          "colourTertiary": "#050d05"
         },
         "data": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#b35900",
+          "colourSecondary": "#8c4600",
+          "colourTertiary": "#663300"
         },
         // This is not a new category, but rather for differentiation
         // between lists and scalar variables.
         "data_lists": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#993300",
+          "colourSecondary": "#732600",
+          "colourTertiary": "#4d1900"
         },
         "more": {
-          "colourPrimary": "#235c23",
-          "colourSecondary": "#235c23",
-          "colourTertiary": "#235c23"
+          "colourPrimary": "#99004d",
+          "colourSecondary": "#730039",
+          "colourTertiary": "#4d0026"
       },
       "text": {
         "colourPrimary": "#FFFFFF",
